bced73c9d70db991917396d496fd6ab780f7bf96
JauniKapauni <riches-goods.54@icloud.com>
2026-05-15 13:11:05 +0200

    feat: add Persistence

--- src/main/java/de/jaunikapauni/axnpcs/AxNPCs.java
+++ src/main/java/de/jaunikapauni/axnpcs/AxNPCs.java
@@ -1,6 +1,8 @@
 package de.jaunikapauni.axnpcs;
 
 import de.jaunikapauni.axnpcs.command.CreateCommand;
+import org.bukkit.Location;
+import org.bukkit.entity.Villager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxNPCs extends JavaPlugin {
@@ -8,11 +10,32 @@ public final class AxNPCs extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getCommand("create").setExecutor(new CreateCommand());
+        saveDefaultConfig();
+        getCommand("create").setExecutor(new CreateCommand(this));
+        loadNpcs();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void loadNpcs(){
+        if(getConfig().getConfigurationSection("npcs") == null){
+            return;
+        }
+        for(String key : getConfig().getConfigurationSection("npcs").getKeys(false)){
+            String path = "npcs." + key + ".";
+
+            Location loc = getConfig().getLocation(path + "location");
+            String name = getConfig().getString(path + "name");
+
+            if(loc != null){
+                Villager npc = loc.getWorld().spawn(loc, Villager.class);
+                npc.setAI(false);
+                npc.setInvulnerable(true);
+                npc.setCustomName(name);
+            }
+        }
     }
 }
--- src/main/java/de/jaunikapauni/axnpcs/command/CreateCommand.java
+++ src/main/java/de/jaunikapauni/axnpcs/command/CreateCommand.java
@@ -1,5 +1,6 @@
 package de.jaunikapauni.axnpcs.command;
 
+import de.jaunikapauni.axnpcs.AxNPCs;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,7 +8,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class CreateCommand implements CommandExecutor {
+    AxNPCs reference;
+    public CreateCommand(AxNPCs reference){
+        this.reference = reference;
+    }
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         Player p = (Player) sender;
@@ -19,6 +26,16 @@ public class CreateCommand implements CommandExecutor {
             return false;
         }
         npc.setCustomName(args[0]);
+        npc.setCustomNameVisible(true);
+
+        String id = UUID.randomUUID().toString();
+        String path = "npcs."+ id;
+
+        reference.getConfig().set(path + ".name", args[0]);
+        reference.getConfig().set(path + ".location", p.getLocation());
+        reference.saveConfig();
+
+        p.sendMessage("NPC created and saved!");
         return true;
     }
 }
