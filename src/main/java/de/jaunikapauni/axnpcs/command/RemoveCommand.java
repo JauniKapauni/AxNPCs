831abf7a50b18cad1c2ff1148340016880f97009
JauniKapauni <riches-goods.54@icloud.com>
2026-07-17 14:20:19 +0200

    fix(command): prevent exception

--- src/main/java/de/jaunikapauni/axnpcs/command/RemoveCommand.java
+++ src/main/java/de/jaunikapauni/axnpcs/command/RemoveCommand.java
@@ -19,6 +19,10 @@ public class RemoveCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if(!(sender instanceof Player)){
+            sender.sendMessage("Only players can run this command!");
+            return true;
+        }
         Player p = (Player) sender;
         if(!p.hasPermission("axnpcs.remove")){
             p.sendMessage("You don't have the permission! [axnpcs.remove]");
